9041fc5cb159eecf9f99d482fc667176a92306ed
Aaron <aaron.hsu86+github@gmail.com>
2023-08-02 23:24:17 -0700

    first commit, no level 2

---
+++ Controllers/DojoSurveyController.cs
@@ -0,0 +1,44 @@
+// This brings all the MVC features we need to this file
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace Dojo_Survey.Controllers;
+public class DojoSurveyController : Controller   // Remember inheritance?    
+{
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(
+        string name,
+        string location,
+        string language,
+        string comment
+    )
+    {
+        Console.WriteLine("Process");
+        ViewBag.Test = "test";
+        return RedirectToAction("Result");
+    }
+
+    // [HttpGet("results")]
+    
+
+    [HttpPost("results")]
+    public ViewResult Result(
+        string name,
+        string location,
+        string language,
+        string comment
+    )
+    {
+        Console.WriteLine("Results");
+        ViewBag.Name = name;
+        ViewBag.Location = location;
+        ViewBag.Language = language;
+        ViewBag.Comment = comment;
+        return View();
+    }
+}
